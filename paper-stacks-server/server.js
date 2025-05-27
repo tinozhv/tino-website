fa9af01dbf4a08f9a20a89e6ff37cb25cf9607c2
tinozhv <tinoschincariol@icloud.com>
2025-05-27 13:46:21 -0400

    editing for render

--- paper-stacks-server/server.js
+++ paper-stacks-server/server.js
@@ -1,14 +1,14 @@
-// 1️⃣ Import the express library
+//Import the express library
 const express = require('express');
 
-// 2️⃣ Create an Express application
+//Create an Express application
 const app = express();
 
-// 3️⃣ Define the port the server will listen on (localhost:3000)
-const port = 3000;
+//Define the port the server will listen on (localhost:3000)
+const port = process.env.PORT || 3000;
 
-// 4️⃣ Set up a simple in-memory stack (array) with placeholder image names
-let stacks = {
+//Set up a simple in-memory stack (array) with placeholder image names
+let fullStacks = {
     left: [
         'https://tinoschincariol.com/paper-stacks/images/left/-%20[%20CA1078%20FLIGHT%20FROM%20JFK%20TO%20ANC.png',
         'https://tinoschincariol.com/paper-stacks/images/left/-%20[%20]%20First%20principle%20and%20the%20very%20first%20beginning.png',
@@ -43,23 +43,72 @@ let stacks = {
         //TODO: add right files
         'https://tinoschincariol.com/paper-stacks/images/right/Affordable%20cinematographer%20for%20hire.png',
         'https://tinoschincariol.com/paper-stacks/images/right/All%20of%20This%20Self-Monitoring.png',
+        'https://tinoschincariol.com/paper-stacks/images/right/Create%20art%20text.png',
+        'https://tinoschincariol.com/paper-stacks/images/right/Driving%20around%20Montreal%20at%203-00%20am.png',
+        'https://tinoschincariol.com/paper-stacks/images/right/Egg%20tempera%20on%20untempered%20hardboard.png',
+        'https://tinoschincariol.com/paper-stacks/images/right/Eurasian%20curlew.png',
+        'https://tinoschincariol.com/paper-stacks/images/right/Floods%20snarl%20traffic%20in%20metropolitan%20area.png',
+        'https://tinoschincariol.com/paper-stacks/images/right/It%20was%20all%20cut%20to%20ribbons.png',
+        'https://tinoschincariol.com/paper-stacks/images/right/Kino%20opened%20his%20hand%20and%20held%20it%20out.png',
+        'https://tinoschincariol.com/paper-stacks/images/right/November%2023,%202024.png',
+        'https://tinoschincariol.com/paper-stacks/images/right/Packaging%20for%20glycerin%20soap.png',
+        'https://tinoschincariol.com/paper-stacks/images/right/Packaging%20for%20glycerin%20soap.png',
+        'https://tinoschincariol.com/paper-stacks/images/right/Pyramids%20should%20be%20stepped.png',
+        'https://tinoschincariol.com/paper-stacks/images/right/The%20Biltmore%20hotel.png',
+        'https://tinoschincariol.com/paper-stacks/images/right/The%20camera%20and%20the%20exhibition.png',
+        'https://tinoschincariol.com/paper-stacks/images/right/The%20end%20is%20a%20bit%20disturbing.png',
+        'https://tinoschincariol.com/paper-stacks/images/right/The%20four%20seas%20rise%20high.png',
+        'https://tinoschincariol.com/paper-stacks/images/right/There%20were%20no%20real%20fruit%20sours.png',
+        'https://tinoschincariol.com/paper-stacks/images/right/This%20passport%20is%20valid%20for%20all%20countries.png',
+        'https://tinoschincariol.com/paper-stacks/images/right/Up%20Colbourne%20Lodge%20Dr.png',
+        'https://tinoschincariol.com/paper-stacks/images/right/Vulcano-Porto.png',
+        'https://tinoschincariol.com/paper-stacks/images/right/Write%20it%20down%20and%20keep%20it%20safe.png',
+        'https://tinoschincariol.com/paper-stacks/images/right/You%20just%20felt%20a%20bit%20ashamed.png',
+        'https://tinoschincariol.com/paper-stacks/images/right/Your%20desires%20are%20not%20extravagant.png',
+        'https://tinoschincariol.com/paper-stacks/images/right/ambient.png'
     ]
-}
+};
 
-// 5️⃣ Define a route: GET /stack/top
+let stacks = {
+    left: shuffleArray(fullStacks.left),
+    right: shuffleArray(fullStacks.right)
+};
+
+function shuffleArray(array) {
+    const copy = [...array];
+    for (let i = copy.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [copy[i], copy[j] = [copy[j], copy[i]]];
+    }
+
+    return copy;
+};
+
+function scheduleDailyReset() {
+    setInterval(() => {
+        const now = new Date();
+        if (now.getUTCHours() === 0 && now.getUTCMinutes() === 0) {
+            stacks.L = shuffleArray(fullStacks.L);
+            stacks.R = shuffleArray(fullStacks.R);
+            console.log('Stacks reset at midnight UTC!');
+        }
+    }, 60 * 1000);  // check every 60 seconds
+};
+
+// Define a route: GET /stack/top
 // This route responds with the top item of the stack (last item in the array)
 app.get('/stack/top', (req, res) => {
     if (stack.length === 0) {
-        // If the stack is empty, return a 404 error with a message
+        // If the stack is empty, return a message
         return res.send('This stack is empty');
     }
     // Otherwise, return the top item
-    res.send(`Top of stack: ${stack[stack.length - 1]}`);
+    res.send(stack[stack.length - 1]);
 });
 
-app.post()
-
-// 6️⃣ Start the Express server and listen for incoming requests on the defined port
+//Start the Express server and listen for incoming requests on the defined port
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
+scheduleDailyReset();
